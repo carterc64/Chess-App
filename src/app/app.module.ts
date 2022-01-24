08a18ed8df998a1ad51b546530d50375b34e4400
carte <cwc00001@mix.wvu.edu>
2022-01-23 21:26:37 -0500

    Starting Setup html = hello world

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './Navigation/topnavbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopNavBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
