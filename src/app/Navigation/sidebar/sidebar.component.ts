0e0240f111921875960329c75e579038c090b743
carte <cwc00001@mix.wvu.edu>
2022-01-26 16:04:36 -0500

    Sidenavbar completed

--- src/app/Navigation/sidebar/sidebar.component.ts
+++ src/app/Navigation/sidebar/sidebar.component.ts
@@ -4,9 +4,10 @@ import { Component } from "@angular/core";
     
 @Component({
     selector: "chess-sidebar",
-    templateUrl: "sidebar.component.html"
+    templateUrl: "sidebar.component.html",
+    styleUrls: ["sidebar.component.css"]
 })
 
 export class SidebarCompnent{
-    
+
 }
