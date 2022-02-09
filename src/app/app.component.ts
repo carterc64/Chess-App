93f3d53cd948b3e41c84ea41d862d2cb3e0ce696
carte <cwc00001@mix.wvu.edu>
2022-02-09 17:40:40 -0500

    Tiny bit of polish plus add comments to new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { mock_news_list1 } from './MainPage/DataboundComponent/mock-news-list1';
 import { NewsItemModel } from './MainPage/DataboundComponent/news-item.model';
 import { mock_news_list2 } from './MainPage/DataboundComponent/mock-news-list2';
-import { NewsItemModel2 } from './MainPage/DataboundComponent/news-item2.model';
+
 
 
 @Component({
@@ -12,20 +12,25 @@ import { NewsItemModel2 } from './MainPage/DataboundComponent/news-item2.model';
 })
 export class AppComponent {
   title = 'Chess-App';
-  products: NewsItemModel [] = [];
-  products2: NewsItemModel2 [] = [];
 
-  constructor(){
-    for (var product of mock_news_list1){
+  /*First two news cards*/
+  products: NewsItemModel[] = [];
+  /*Second two news cards*/
+  products2: NewsItemModel[] = [];
+
+
+  constructor() {
+    /*First two*/
+    for (var product of mock_news_list1) {
       console.log(product);
       this.products.push(product);
     }
-
-    for (var product2 of mock_news_list2){
+    /*last two*/
+    for (var product2 of mock_news_list2) {
       console.log(product2);
       this.products2.push(product2);
     }
   }
 
- 
+
 }
