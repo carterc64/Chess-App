04a7c41bb5c06a1d5d254159c0c51f81c82b577d
Carterc64 <cwc00001@mix.wvu.edu>
2022-02-08 19:07:58 -0500

    Added databound card component

---
+++ src/app/MainPage/databound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'databound-card',
+    templateUrl: 'databound.component.html',
+    styleUrls: ['databound.component.css']
+})
+export class DataboundCardComponent{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataboundCardComponent } from './MainPage/databound.component';
 import { VerticalCardComponent } from './MainPage/vertical.card.component';
 import { VerticalCardComponent2 } from './MainPage/vertical.card.component2';
 import { VerticalCardComponent3 } from './MainPage/vertical.card.component3';
@@ -16,7 +17,8 @@ import { SidebarCompnent } from './Navigation/sidebar/sidebar.component';
     FooterComponent,
     VerticalCardComponent,
     VerticalCardComponent2,
-    VerticalCardComponent3
+    VerticalCardComponent3,
+    DataboundCardComponent
   ],
   imports: [
     BrowserModule
